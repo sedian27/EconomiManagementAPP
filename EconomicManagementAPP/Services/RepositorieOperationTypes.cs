2403fc4221164c85805a6d1ff8844554d426df66
sedian <diestrasergio@gmail.com>
2022-03-25 09:41:12 -0500

    Methods in RepositorieOperationTypes added

--- EconomicManagementAPP/Services/RepositorieOperationTypes.cs
+++ EconomicManagementAPP/Services/RepositorieOperationTypes.cs
@@ -1,6 +1,68 @@
-﻿namespace EconomicManagementAPP.Services
+﻿using Dapper;
+using EconomicManagementAPP.Models;
+using Microsoft.Data.SqlClient;
+
+namespace EconomicManagementAPP.Services
 {
-    public class RepositorieOperationTypes
+    public interface IRepositorieOperationTypes
     {
+        Task Create(OperationTypes operationType);
+        Task<bool> Exist(string description);
+        Task<OperationTypes> GetOperationTypeById(int id);
+        Task<IEnumerable<OperationTypes>> GetOperationTypes();
+        Task Modify(OperationTypes operationTypes);
+        Task Delete(int id);
+    }
+    public class RepositorieOperationTypes : IRepositorieOperationTypes
+    {
+        private readonly string connectionString;
+        public RepositorieOperationTypes(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        public async Task Create(OperationTypes operationType)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>(@"INSERT INTO OperationTypes 
+                                                            (Description) VALUES
+                                                            (@Description); 
+                                                            SELECT SCOPE_IDENTITY();", operationType);
+            operationType.Id = id;
+        }
+        public async Task<bool> Exist(string description)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var exist = await connection.QueryFirstOrDefaultAsync<int>(@"SELECT 1 FROM OperationTypes
+                                                                        WHERE Description = @description",
+                                                                        new { description });
+            return exist == 1;
+        }
+        public async Task<OperationTypes> GetOperationTypeById(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            return await connection.QueryFirstOrDefaultAsync<OperationTypes>(@"SELECT Id, Description
+                                                                               FROM OperationTypes
+                                                                               WHERE Id = @id",
+                                                                               new { id });
+        }
+        public async Task<IEnumerable<OperationTypes>> GetOperationTypes()
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            return await connection.QueryAsync<OperationTypes>("SELECT Id, Description FROM OperationTypes");
+        }
+        public async Task Modify(OperationTypes operationTypes)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(@"UPDATE OperationTypes SET 
+                                            Description = @Description
+                                            WHERE Id = @Id", operationTypes);
+        }
+        public async Task Delete(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync("DELETE OperationTypes WHERE Id = @Id", new { id });
+        }
     }
 }
